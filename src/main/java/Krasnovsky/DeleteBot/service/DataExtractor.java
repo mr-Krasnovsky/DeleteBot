d2d354e61974db98f5889ae184a03f7a153ce81e
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-08-10 14:20:53 +0300

    the basic functionality has been created, the constants have been moved to the properties file

--- src/main/java/Krasnovsky/DeleteBot/service/DataExtractor.java
+++ src/main/java/Krasnovsky/DeleteBot/service/DataExtractor.java
@@ -4,6 +4,8 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.MessageEntity;
@@ -11,7 +13,11 @@ import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 public class DataExtractor {
 	private static boolean urlStatus = false;
 
+	private static final String URL_REGEX = "https?://\\S+";
+	private static final Pattern URL_SYMBOLS = Pattern.compile(URL_REGEX);
+
 	public boolean extractData(Message message) {
+
 		Map<String, String> data = new HashMap<>();
 		data.put("url", "<N/A>");
 		data.put("email", "<N/A>");
@@ -41,12 +47,16 @@ public class DataExtractor {
 			int captionLenth = message.getCaption().length();
 			System.out.println(message.getCaption() + " caption");
 			for (MessageEntity entity : message.getCaptionEntities()) {
+
+				System.out.println("message.entity.text: " + entity.getType());
+
 				if (entity.getType().equals("url") && entity.getOffset() != null && entity.getLength() != null) {
-					String url = entity.getText().substring(entity.getOffset(),
-							entity.getOffset() + entity.getLength());
-					String decodedUrl = URLDecoder.decode(url, StandardCharsets.UTF_8);
-					urlStatus = decodedUrl.length() == captionLenth ? true : false;
-					data.put("url", decodedUrl);
+					String text = entity.getText();
+					if (urlStatus = containsUrl(text) == true) {
+						data.put("url", text);
+					} else {
+						urlStatus = false;
+					}
 				} else if (entity.getType().equals("email") && entity.getOffset() != null
 						&& entity.getLength() != null) {
 					String email = entity.getText().substring(entity.getOffset(),
@@ -66,7 +76,14 @@ public class DataExtractor {
 
 		// Отправка ответного сообщения
 		// replace "message.reply(reply)" with your actual implementation
-		System.out.println(reply + "\n" + urlStatus + "urlSTATUS");
+		System.out.println("urlSTATUS: " + urlStatus);
 		return urlStatus;
 	}
+
+	private boolean containsUrl(String text) {
+		Pattern pattern = Pattern.compile(URL_REGEX);
+		Matcher matcher = pattern.matcher(text);
+		return matcher.find();
+	}
+
 }
--- src/main/java/Krasnovsky/DeleteBot/service/DeleteCommentsBot.java
+++ src/main/java/Krasnovsky/DeleteBot/service/DeleteCommentsBot.java
@@ -3,195 +3,303 @@ package Krasnovsky.DeleteBot.service;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.groupadministration.GetChatMember;
+import org.telegram.telegrambots.meta.api.methods.groupadministration.LeaveChat;
+import org.telegram.telegrambots.meta.api.methods.groupadministration.RestrictChatMember;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import Krasnovsky.DeleteBot.config.BotConfig;
+import Krasnovsky.DeleteBot.config.BotConstants;
+import lombok.Getter;
 
+@Getter
 @Component
 public class DeleteCommentsBot extends TelegramLongPollingBot {
 
-	private static long CHANNEL_ID = -1001889571875L;
-	private static long BOSS_ID = 163862455;
-	private static long Father_ID = 241147591;
+    private static long CHANNEL_ID = BotConstants.CHANNEL_ID_PARK;
+    private static long TEST_ID = BotConstants.TEST_ID;
+    private static long MONEY_CHANNEL_ID = BotConstants.MONEY_CHANNEL_ID;
+    private static long BUSINESS_CHANNEL_ID = BotConstants.BUSINESS_CHANNEL_ID;
+    private static long BUSINESS_CHAT_ID = BotConstants.BUSINESS_CHAT_ID;
+    private static long BOSS_ID = BotConstants.BOSS_ID;
+    private static long FATHER_ID = BotConstants.FATHER_ID;
+    private static final String URL_REGEX = BotConstants.URL_REGEX;
+    private static final Pattern URL_SYMBOLS = Pattern.compile(URL_REGEX);
 
-	private static final String URL_REGEX = "https?://\\S+";
+    final BotConfig config;
 
-	final BotConfig config;
+    @SuppressWarnings("deprecation")
+    public DeleteCommentsBot(BotConfig config) {
+	this.config = config;
+    }
 
-	public DeleteCommentsBot(BotConfig config) {
-		this.config = config;
-	}
+    @Override
+    public String getBotUsername() {
 
-	@Override
-	public String getBotUsername() {
+	return config.getBotName();
+    }
 
-		return config.getBotName();
-	}
+    @Override
+    public String getBotToken() {
 
-	@Override
-	public String getBotToken() {
+	return config.getToken();
+    }
 
-		return config.getToken();
-	}
+    @Override
+    public void onUpdateReceived(Update update) {
 
-	@Override
-	public void onUpdateReceived(Update update) {
+	System.out.println("MESSAGE: " + update.getMessage().toString());
 
-		if (update.hasMessage()) {
+	if (update.hasMessage()) {
 
-			Message message = update.getMessage(); // Получаем новое сообщение
-			Long userId = message.getFrom().getId(); // Получаем USER_ID отправителя
+	    Message message = update.getMessage(); // Получаем новое сообщение
 
-			if (userId != BOSS_ID && userId != Father_ID) { // Отсекаем сообщения автора канала
+	    long userId = message.getFrom().getId();
+	    System.out.println("//// USER_ID " + userId);
+	    CHANNEL_ID = message.getChatId();// Получаем CHAT_ID отправителя
+	    if (CHANNEL_ID != MONEY_CHANNEL_ID && CHANNEL_ID != BUSINESS_CHANNEL_ID && CHANNEL_ID != TEST_ID
+		    && CHANNEL_ID != BUSINESS_CHAT_ID) {
+		leaveGroup(CHANNEL_ID);
+	    }
+	    // if (userId != BOSS_ID) { // Исключаем сообщения автора && userId != Father_ID
+	    // канала
 
-				Instant instant = Instant.ofEpochSecond(message.getDate());
-				LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault()); // получаем дату
-																									// сообщения
+	    Instant instant = Instant.ofEpochSecond(message.getDate()); // получаем дату
+	    LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault()); // сообщения //
+											       // сообщения
+	    // GetChatMember getChatMember = new
+	    // GetChatMember(message.getChatId().toString(), userId);
+	    // Integer joinDate = chatMember.getDate();
+	    // long joinDateMillis = (long) joinDate * 1000;
+//				System.out.println(message.getText() + " TEXT " + "\n\n");
+//				System.out.println(message + " sender " + userId + "\n\n");
+//				System.out.println(message.getChatId());
 
-				GetChatMember getChatMember = new GetChatMember(message.getChatId().toString(), userId);
+	    boolean urlStatus = checkUrlStatus(message); // есть ли в сообщении URL
+	    System.out.println(urlStatus);
 
-				String member = getChatMember.getMethod();
-				// Integer joinDate = chatMember.getDate();
-				// long joinDateMillis = (long) joinDate * 1000;
-
-				System.out.println(message + " sender " + userId + "\n\n" + " date " + dateTime);
-				System.out.println(message.getChatId());
-
-				boolean urlStatus = checkUrlStatus(message);
-				if (message.getText() != null) {
-					String messageText = message.getText();
-					checkMessage(messageText, urlStatus);
-				} else if (message.hasPhoto() && message.getText() == null) {
-					checkPaint(message, urlStatus);
-				} else if (message.isReply()) {
-					Message repliedMessage = message.getReplyToMessage();
-					System.out.println(repliedMessage);
-				}
-			} else if (update.hasChannelPost()) {
-				if (update.getChannelPost().getChatId() == CHANNEL_ID) {
-					Message message1 = update.getChannelPost();
-					boolean urlStatus = checkUrlStatus(message1);
-					if (message1.getText() != null) {
-						String messageText = message1.getText();
-						checkMessage(messageText, urlStatus);
-					} else if (message1.hasPhoto() && message1.getText() == null) {
-						checkPaint(message1, urlStatus);
-					}
-				}
-			}
+	    if (message.hasPhoto() && message.getText() == null) {
+		if (checkPaint(message, urlStatus) && userId != 0 && message.getChatId() != null) {
+		    // blockUser(CHANNEL_ID, userId);
 		}
-	}
-
-	private static final Pattern PUNCT_SYMBOLS = Pattern.compile("[!\"#$%&'()*+,-./:;<«=»>4⃣?@\\[\\]^_`{|}~]\\n");
-	private static final Pattern URL_SYMBOLS = Pattern.compile(URL_REGEX);
-	private static final Pattern SMILES = Pattern.compile(
-			"[\\ud800-\\udbff\\udc00-\\udfff\\ud83c\\ud000-\\udfff" + "\\ud83d\\ud000-\\udfff\\ud83e\\ud000-\\udfff]");
-
-	public static String removePunctuations(String source) {
-		return PUNCT_SYMBOLS.matcher(source).replaceAll("");
-	}
-
-	public static String removeUrl(String source) {
-		return URL_SYMBOLS.matcher(source).replaceAll("");
-	}
-
-	public static String removeSmiles(String source) {
-		return SMILES.matcher(source).replaceAll("");
-	}
-
-	private void checkMessage(String messageText, boolean urlStatus) {
-		messageText = removeUrl(messageText);
-		messageText = removePunctuations(messageText);
-		messageText = removeSmiles(messageText);
-
-		String[] words = messageText.split(" ");
-		Pattern cyrillic = Pattern.compile("^[а-яА-Я]+$");
-		Pattern latin = Pattern.compile("^[a-zA-Z]+$");
-		Matcher mCyrillic;
-		Matcher mLatin;
-
-		for (String word : words) {
-			mCyrillic = cyrillic.matcher(word);
-			mLatin = latin.matcher(word);
-			if (mCyrillic.find() && !mLatin.find()) {
-				System.out.println(word + " is cyrillic");
-			} else if (!mCyrillic.find() && mLatin.find()) {
-				System.out.println(word + " is latin");
-			} else if (!mCyrillic.find() && !mLatin.find() && word.length() > 1) {
-				System.out.println(word + " is mixed");
-
-			}
+	    } else if (message.getText() != null) {
+		if (checkMessage(message, urlStatus) && urlStatus == true) {
+		    // blockUser(CHANNEL_ID, userId);
 		}
-		/*
-		 * if (!mCyrillic.find() && !mLatin.find()) { c break; }
-		 * 
-		 * }
-		 */
 
-		// if (messageText.contains("τ") || messageText.contains("π") ||
-		// messageText.contains("ρ")
-		// || messageText.contains("u")) {
-		// sendMessage(CHANNEL_ID, "Фу! плохой пользователь TEXT");
-		// }
+	    }
+	    // else if (message.isReply()) {
+//					Message repliedMessage = message.getReplyToMessage();
+//					System.out.println(repliedMessage);
+//
+//				} else if (update.hasChannelPost()) {
+//
+//					if (update.getChannelPost().getChatId() == CHANNEL_ID) {
+//						Message message1 = update.getChannelPost();
+//						urlStatus = checkUrlStatus(message1);
+//						if (message1.getText() != null) {
+//							String messageText = message1.getText();
+//							checkMessage(messageText, urlStatus);
+//						} else if (message1.hasPhoto() && message1.getText() == null) {
+//							checkPaint(message1, urlStatus);
+//						}
+//					}
+//				}
+	    // } BOSS ID
+	    if (userId == 873614042) {
+		deleteMessage(message);
+		sendMessage(CHANNEL_ID, "Фу! пользователь полное Г" + " " + "ID" + " " + userId);
+	    }
 	}
+    }
 
-	private void deleteMessage(Message message) {
-		DeleteMessage deleteMessage = new DeleteMessage(String.valueOf(CHANNEL_ID), message.getMessageId());
-		try {
-			execute(deleteMessage);
-		} catch (TelegramApiException e) {
-			e.printStackTrace();
-		}
+    private static final Pattern PUNCT_SYMBOLS = Pattern.compile("[!\"#$%&'()*+,-./:;<«=»>4⃣?@\\[\\]^_`{|}~]\\n");
+
+    private static final Pattern SMILES = Pattern.compile(
+	    "[\\ud800-\\udbff\\udc00-\\udfff\\ud83c\\ud000-\\udfff" + "\\ud83d\\ud000-\\udfff\\ud83e\\ud000-\\udfff]");
+
+    public static String removePunctuations(String source) {
+	return PUNCT_SYMBOLS.matcher(source).replaceAll("");
+    }
+
+    public static String removeUrl(String source) {
+	return URL_SYMBOLS.matcher(source).replaceAll("");
+    }
+
+    public static String removeSmiles(String source) {
+	return SMILES.matcher(source).replaceAll("");
+    }
+
+    private boolean checkMessage(Message message, boolean urlStatus) {
+	String messageText = message.getText();
+	messageText = removeUrl(messageText);
+	messageText = removePunctuations(messageText);
+	messageText = removeSmiles(messageText);
+
+	List<String> words = Arrays.asList(messageText.split(" "));
+	System.out.println("!!!WORDS :" + words.toString());
+//		Pattern cyrillic = Pattern.compile("^[а-яА-Я]+$");
+//		Pattern latin = Pattern.compile("^[a-zA-Z]+$");
+//		Matcher mCyrillic;
+//		Matcher mLatin;
+//
+//		for (String word : words) {
+//			mCyrillic = cyrillic.matcher(word);
+//			mLatin = latin.matcher(word);
+//			if (mCyrillic.find() && !mLatin.find()) {
+//				System.out.println(word + " is cyrillic");
+//			} else if (!mCyrillic.find() && mLatin.find()) {
+//				System.out.println(word + " is latin");
+//			} else if (!mCyrillic.find() && !mLatin.find() && word.length() > 1) {
+//				System.out.println(word + " is mixed");
+//			}
+//		}
+
+	String regex = "\\p{L}\\p{M}*|\\p{N}+|[\\p{P}\\p{S}]+";
+
+	Pattern pattern = Pattern.compile(regex);
+	Matcher matcher = pattern.matcher(messageText);
+
+//		if (words.contains("Путин") || words.contains("Зеленский") || words.contains("Жириновский")
+//				|| words.contains("Кадры точно не для детей!") || words.contains("Жиринọвскọгọ") && urlStatus == true) {
+	if (matcher.find() && urlStatus == true) {
+	    deleteMessage(message); // удаление сообщения
+	    sendMessage(CHANNEL_ID, "Фу! плохой пользователь" + " " + "TEXT" + " " + urlStatus);
+	    urlStatus = false;
+	    return true;
 	}
+	/*
+	 * if (!mCyrillic.find() && !mLatin.find()) { c break; }
+	 * 
+	 * }
+	 */
+	return false;
 
-	private void checkPaint(Message message, boolean urlStatus) {
+	// if (messageText.contains("τ") || messageText.contains("π") ||
+	// messageText.contains("ρ")
+	// || messageText.contains("u")) {
+	// sendMessage(CHANNEL_ID, "Фу! плохой пользователь TEXT");
+	// }
+    }
 
-		List<PhotoSize> photoList = message.getPhoto(); // Получение информации о фото
-
-		if (!photoList.isEmpty() && urlStatus == true) { // Проверка наличия фото
-			// deleteMessage(message); // удаление сообщения
-			sendMessage(CHANNEL_ID, "Фу! плохой пользователь" + " " + !photoList.isEmpty() + " " + urlStatus);
-			urlStatus = false;
-		} else {
-			checkMessage(message.getCaption(), urlStatus);
-			// sendMessage(CHANNEL_ID, "Хороший пользователь PHOTO" + !photoList.isEmpty() +
-			// urlStatus);
-		}
+    private void deleteMessage(Message message) {
+	DeleteMessage deleteMessage = new DeleteMessage(String.valueOf(CHANNEL_ID), message.getMessageId());
+	try {
+	    execute(deleteMessage);
+	} catch (TelegramApiException e) {
+	    e.printStackTrace();
 	}
+    }
 
-	private boolean containsUrl(String text) {
-		Pattern pattern = Pattern.compile(URL_REGEX);
-		Matcher matcher = pattern.matcher(text);
-		return matcher.find();
+    private void blockUser(Long chatId, Long userId) {
+	RestrictChatMember restrictChatMember = new RestrictChatMember();
+	restrictChatMember.setChatId(chatId);
+	restrictChatMember.setUserId(userId);
+	try {
+	    execute(restrictChatMember);
+	} catch (TelegramApiException e) {
+	    e.printStackTrace();
 	}
+    }
 
-	private boolean checkUrlStatus(Message message) {
-		DataExtractor extractor = new DataExtractor();
-		return extractor.extractData(message);
+    private Boolean checkPaint(Message message, boolean urlStatus) {
+
+	List<PhotoSize> photoList = message.getPhoto(); // Получение информации о фото
+
+	if (!photoList.isEmpty() && urlStatus == true) { // Проверка наличия фото
+	    deleteMessage(message); // удаление сообщения
+	    sendMessage(CHANNEL_ID, "Фу! плохой пользователь PHOTO" + " " + !photoList.isEmpty() + " " + urlStatus);
+	    urlStatus = false;
+	    return true;
+	} else {
+	    sendMessage(CHANNEL_ID, "Хороший пользователь PHOTO" + !photoList.isEmpty() + urlStatus);
+	    return false;
 	}
+    }
 
-	/// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	private void sendMessage(long chatId, String textToSend) {
-		SendMessage message = new SendMessage();
-		message.setChatId(chatId);
-		message.setText(textToSend);
+    private boolean containsUrl(String text) {
+	Pattern pattern = Pattern.compile(URL_REGEX);
+	Matcher matcher = pattern.matcher(text);
+	return matcher.find();
+    }
 
-		try {
-			execute(message);
-		} catch (TelegramApiException e) {
-
-		}
+    private boolean checkUrlStatus(Message message) {
+	List<Boolean> tests = new ArrayList<>();
+	if (message.getText() != null) {
+	    tests.add(message.getText().matches(URL_REGEX));
 	}
+	if (message.getEntities() != null) {
+	    for (MessageEntity entity : message.getEntities()) {
+		tests.add(entity.getUrl() != null);
+		tests.add(entity.getText().matches(URL_REGEX));
+	    }
+	}
+	if (message.getCaptionEntities() != null) {
+	    for (MessageEntity entity : message.getCaptionEntities()) {
+		tests.add(entity.getUrl() != null);
+		tests.add(entity.getText().matches(URL_REGEX));
+	    }
+	}
+	System.out.println(tests);
+	System.out.println("URLS :" + tests);
+	if (tests.contains(true)) {
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+
+    /// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    private void sendMessage(long chatId, String textToSend) {
+	SendMessage message = new SendMessage();
+	message.setChatId(TEST_ID);
+	message.setText(textToSend);
+
+	try {
+	    execute(message);
+	} catch (TelegramApiException e) {
+
+	}
+    }
+
+    public void leaveGroup(long groupId) {
+	LeaveChat leaveChat = new LeaveChat();
+	leaveChat.setChatId(groupId);
+
+	try {
+	    execute(leaveChat);
+	    System.out.println("Бот покинул группу");
+	} catch (TelegramApiException e) {
+	    e.printStackTrace();
+	}
+    }
 }
+/// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! отправка
+/// сообщения в чат
+//	public void sendPostWithImage(long groupId, String imagePath) {
+//		SendPhoto sendPhoto = new SendPhoto();
+//		sendPhoto.setChatId(groupId);
+//		sendPhoto.setPhoto(new InputFile(new File(imagePath)));
+//		sendPhoto.setCaption("");
+//
+//		try {
+//			// Отправка поста с изображением
+//			Message sentMessage = execute(sendPhoto);
+//			System.out.println("Пост с изображением успешно отправлен. ID сообщения: " + sentMessage.getMessageId());
+//		} catch (TelegramApiException e) {
+//			System.out.println("Ошибка при отправке поста с изображением: " + e.getMessage());
+//		}
+//	}
